feb110d7ab57b131c64b70fda270e2a11dc80e16
Igor Vaiman <gosha.vaiman@gmail.com>
2022-05-29 22:29:33 +0400

    utils added

---
+++ telebot_components/utils.py
@@ -0,0 +1,9 @@
+from typing import Optional
+
+
+def telegram_message_url(group_id: int, message_id: int, thread_op_message_id: Optional[int] = None):
+    group_id_str = str(group_id).replace("-100", "")
+    message_url = f"https://t.me/c/{group_id_str}/{message_id}"
+    if thread_op_message_id is not None:
+        message_url += f"?thread={thread_op_message_id}"
+    return message_url
