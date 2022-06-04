26106593ef8ea894d61234bf80bb1dd80f5671c3
Igor Vaiman <gosha.vaiman@gmail.com>
2022-06-04 15:36:05 +0400

    telegram api mocck util added

--- tests/utils.py
+++ tests/utils.py
@@ -1,7 +1,11 @@
 import os
+from typing import Any, Callable
+import aiohttp
+from aioresponses import CallbackResult
 
 import pytest
 import pytest_mock
+from yarl import URL
 
 
 class TimeSupplier:
@@ -33,3 +37,21 @@ pytest_skip_on_real_redis = pytest.mark.skipif(using_real_redis(), reason="Can't
 
 def mock_bot_user_json() -> dict[str, str]:
     return {"id": 124521435, "is_bot": True, "first_name": "this bot", "username": "something"}
+
+
+def telegram_api_mock(form_data_handler: Callable[[dict[str, str]], dict[str, Any]]):
+    """Used to create callback for aioresponses"""
+    
+    def callback(url: URL, data: aiohttp.FormData, **kwargs):
+        # parsing aiohttp form format to dict
+        form_data = dict()
+        for mdict, _, dump in data._fields:
+            form_data[mdict["name"]] = dump
+        return CallbackResult(
+            status=200, payload={
+                "ok": True,
+                "result": form_data_handler(form_data),
+            },
+        )
+
+    return callback
