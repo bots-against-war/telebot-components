7a08cabb8b102be9f9858c932d40042025974275
Igor Vaiman <gosha.vaiman@gmail.com>
2022-06-04 11:45:33 +0400

    utils covered with tests

--- telebot_components/utils.py
+++ telebot_components/utils.py
@@ -1,15 +1,40 @@
-from typing import Optional
+from typing import Optional, Union
 
 
-def telegram_message_url(group_id: int, message_id: int, thread_op_message_id: Optional[int] = None):
-    group_id_str = str(group_id).replace("-100", "")
-    message_url = f"https://t.me/c/{group_id_str}/{message_id}"
+def telegram_message_url(
+    chat_id: Union[int, str],
+    message_id: int,
+    thread_op_message_id: Optional[int] = None,
+    comment_message_id: Optional[int] = None,
+) -> str:
+    """Note: there are two ways to link to a comment to a channel post:
+    1. Entirely from discussion chat:
+        - chat_id = discussion chat id (-100xxxxxxxxxxx)
+        - message_id = comment's id
+        - thread_op_message_id = id of a channel post's duplicate in discussion channel
+    2. From both channel and discussion chat
+        - chat_id = channel handle (@my_channel)
+        - message_id = channel post's id
+        - comment_message_id = comment's id **in discussion chat**
+    """
+    if thread_op_message_id is not None and comment_message_id is not None:
+        raise ValueError("thread and comment can't be used together")
+    if isinstance(chat_id, int):
+        chat_id_route = str(chat_id).replace("-100", "")
+        chat_id_route = f"c/{chat_id_route}"
+    else:
+        chat_id_route = chat_id.strip("@ ")
+    message_url = f"https://t.me/{chat_id_route}/{message_id}"
     if thread_op_message_id is not None:
         message_url += f"?thread={thread_op_message_id}"
+    if comment_message_id is not None:
+        message_url += f"?comment={comment_message_id}"
     return message_url
 
 
 def trim_with_ellipsis(message: str, target_len: int) -> str:
+    if len(message) <= target_len:
+        return message
     words = []
     current_len = 0
     for word in message.split():
@@ -21,4 +46,4 @@ def trim_with_ellipsis(message: str, target_len: int) -> str:
 
 
 def join_paragraphs(lines: list[str]) -> str:
-    return "\n\n".join(lines)
+    return "\n\n".join([l for l in lines if l])
---
+++ tests/test_utils.py
@@ -0,0 +1,107 @@
+from typing import Optional, Union
+import pytest
+from telebot_components.utils import join_paragraphs, telegram_message_url, trim_with_ellipsis
+
+
+@pytest.mark.parametrize(
+    "paragraphs, expected_joined",
+    [
+        pytest.param(["a", "b"], "a\n\nb"),
+        pytest.param(["hello", "", "", ""], "hello"),
+        pytest.param(["", "", "foo", ""], "foo"),
+        pytest.param(["", "", "", ""], ""),
+    ],
+)
+def test_join_paragraphs(paragraphs: list[str], expected_joined: str):
+    assert join_paragraphs(paragraphs) == expected_joined
+
+
+@pytest.mark.parametrize(
+    "chat_id, message_id, thread_op_message_id, comment_message_id, expected_url",
+    [
+        pytest.param(
+            -1009510656010,
+            5724,
+            None,
+            None,
+            "https://t.me/c/9510656010/5724",
+            id="supergroup message link",
+        ),
+        pytest.param(
+            "@my_channel_handle",
+            1312,
+            None,
+            None,
+            "https://t.me/my_channel_handle/1312",
+            id="channel post link",
+        ),
+        pytest.param(
+            "@my_channel_handle",
+            1312,
+            None,
+            5763,
+            "https://t.me/my_channel_handle/1312?comment=5763",
+            id="channel post comment (1st way)",
+        ),
+        pytest.param(
+            -1009510656010,
+            9000,
+            8440,
+            None,
+            "https://t.me/c/9510656010/9000?thread=8440",
+            id="channel post comment (2nd way)",
+        ),
+    ],
+)
+def test_telegram_message_url(
+    chat_id: Union[int, str],
+    message_id: int,
+    thread_op_message_id: Optional[int],
+    comment_message_id: Optional[int],
+    expected_url: str,
+):
+    assert telegram_message_url(chat_id, message_id, thread_op_message_id, comment_message_id) == expected_url
+
+
+def test_telegram_message_url_mutually_exclusive_params():
+    with pytest.raises(ValueError):
+        telegram_message_url("@something", 1312, 555, 145)
+
+
+@pytest.mark.parametrize(
+    "message, target_len, expected_trimmed",
+    [
+        pytest.param(
+            "Lorem ipsum dolor sit amet",
+            9,
+            "Lorem ipsum...",
+        ),
+        pytest.param(
+            "    Lorem ipsum dolor sit",
+            7,
+            "Lorem ipsum...",
+            id="spaces are stripped before trimming text",
+        ),
+        pytest.param(
+            "  Lorem      ipsum dolor sit",
+            14,
+            "Lorem ipsum dolor...",
+            id="multiple spaces between words are collapsed before trimming text",
+        ),
+        pytest.param("Lorem ipsum dolor sit amet", 1, "Lorem...", id="trim only on word boundaries"),
+        pytest.param(
+            "Lorem ipsum dolor sit amet",
+            1000,
+            "Lorem ipsum dolor sit amet",
+            id="no ellipsis if the whole message is inside target len",
+        ),
+        pytest.param(
+            "   Lorem    ipsum dolor sit  amet     ",
+            1000,
+            "   Lorem    ipsum dolor sit  amet     ",
+            id="no whitesspace stripping and collapsing when no trimming is done",
+        ),
+    ],
+)
+def test_trim_with_ellipsis(message: str, target_len: int, expected_trimmed: str):
+    assert trim_with_ellipsis(message, target_len) == expected_trimmed
