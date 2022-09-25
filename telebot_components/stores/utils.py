ab9e4f11cc2e4483c11831b56800bfb3bce89414
Igor Vaiman <gosha.vaiman@gmail.com>
2022-09-25 22:18:30 +0400

    another annoying error fixes

--- telebot_components/stores/utils.py
+++ telebot_components/stores/utils.py
@@ -9,11 +9,15 @@ async def callback_query_processing_error(
     call: types.CallbackQuery,
     details: str,
     logger: Optional[logging.Logger] = None,
+    error_level: bool = True,
 ):
     if logger is not None:
-        logger.exception(details)
+        if error_level:
+            logger.exception(details)
+        else:
+            logger.info(details, exc_info=True)
     await bot.answer_callback_query(
         call.id,
-        f"Server error: {details}. Try refreshing menu buttons.",
+        f"Server error: {details}. Please refresh menu buttons (e.g. send /start command again).",
         show_alert=True,
     )
