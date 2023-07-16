70c741a00650e339181d08d427ec0f1adbfa6594
Igor Vaiman <gosha.vaiman@gmail.com>
2023-07-16 16:06:01 +0400

    COMP-40: tests added

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -12,6 +12,9 @@ from telebot_components.menu.menu import (
     MenuItem,
     TerminatorContext,
 )
+from telebot_components.redis_utils.interface import RedisInterface
+from telebot_components.stores.language import Language, LanguageStore
+from tests.utils import extract_full_kwargs
 
 
 @pytest.mark.parametrize(
@@ -34,12 +37,13 @@ def test_menu_item_validation(submenu, terminator, link_url):
 @pytest.fixture
 def example_menu() -> Menu:
     return Menu(
-        text="example menu",
+        text="example menu =<^_^>=",
         menu_items=[
             MenuItem(
                 label="picking game",
                 submenu=Menu(
-                    text="what do you want to pick?",
+                    text="<b>what do you want to pick?</b>",
+                    config=MenuConfig(is_text_html=True, back_label="<-", lock_after_termination=False),
                     menu_items=[
                         MenuItem(
                             label="color",
@@ -84,8 +88,6 @@ async def test_menu_handler_basic(example_menu: Menu):
     )
 
     example_user = tg.User(id=1312, is_bot=False, first_name="Max", last_name="Slater")
-    language = await menu_handler.get_maybe_language(example_user)
-
     seen_terminators: List[str] = []
     terminator_context_check_failed = False
 
@@ -98,13 +100,30 @@ async def test_menu_handler_basic(example_menu: Menu):
     menu_handler.setup(bot, on_terminal_menu_option_selected=on_menu_termination)
 
     main_menu = menu_handler.get_main_menu()
-    assert main_menu.text == "example menu"
-    assert main_menu.get_keyboard_markup(language).to_dict() == {
+    assert main_menu.text == "example menu =<^_^>="
+    assert main_menu.get_keyboard_markup(None).to_dict() == {
         "inline_keyboard": [
             [{"text": "picking game", "callback_data": "menu:1"}],
             [{"text": "feedback", "callback_data": "terminator:1"}],
         ]
     }
+
+    await menu_handler.start_menu(bot, example_user)
+    assert len(bot.method_calls) == 1
+    assert extract_full_kwargs(bot.method_calls.pop("send_message")) == [
+        {
+            "chat_id": 1312,
+            "text": "example menu =&amp;lt;^_^&amp;gt;=",  # NOTE: properly escaped for parse mode HTML
+            "reply_markup": tg.InlineKeyboardMarkup(
+                [
+                    [tg.InlineKeyboardButton(text="picking game", callback_data="menu:1")],
+                    [tg.InlineKeyboardButton(text="feedback", callback_data="terminator:1")],
+                ]
+            ),
+            "parse_mode": "HTML",
+        }
+    ]
+    bot.method_calls.clear()
 
     async def press_button(callback_data: str):
         update_json = {
@@ -135,7 +154,9 @@ async def test_menu_handler_basic(example_menu: Menu):
     await press_button("menu:1")
     assert len(bot.method_calls["edit_message_text"]) == 1
     edit_menu_message_method_call = bot.method_calls["edit_message_text"][0]
-    assert edit_menu_message_method_call.full_kwargs["text"] == "what do you want to pick?"
+    assert (
+        edit_menu_message_method_call.full_kwargs["text"] == "<b>what do you want to pick?</b>"
+    )  # not escaped, true html
     assert edit_menu_message_method_call.full_kwargs["reply_markup"].to_dict() == {
         "inline_keyboard": [
             [{"text": "color", "callback_data": "menu:2"}],
@@ -161,7 +182,7 @@ async def test_menu_handler_basic(example_menu: Menu):
     await press_button("menu:1")
     assert len(bot.method_calls["edit_message_text"]) == 3
     edit_menu_message_method_call = bot.method_calls["edit_message_text"][2]
-    assert edit_menu_message_method_call.full_kwargs["text"] == "what do you want to pick?"
+    assert edit_menu_message_method_call.full_kwargs["text"] == "<b>what do you want to pick?</b>"
     assert edit_menu_message_method_call.full_kwargs["reply_markup"].to_dict() == {
         "inline_keyboard": [
             [{"text": "color", "callback_data": "menu:2"}],
@@ -181,7 +202,7 @@ async def test_menu_handler_basic(example_menu: Menu):
     await press_button("menu:0")
     assert len(bot.method_calls["edit_message_text"]) == 4
     edit_menu_message_method_call = bot.method_calls["edit_message_text"][3]
-    assert edit_menu_message_method_call.full_kwargs["text"] == "example menu"
+    assert edit_menu_message_method_call.full_kwargs["text"] == "example menu =&amp;lt;^_^&amp;gt;="
     assert edit_menu_message_method_call.full_kwargs["reply_markup"].to_dict() == {
         "inline_keyboard": [
             [{"text": "picking game", "callback_data": "menu:1"}],
@@ -206,3 +227,101 @@ async def test_menu_handler_basic(example_menu: Menu):
             [{"text": "blue", "callback_data": "inactive_button"}],
         ]
     }
+
+
+async def test_menu_handler_with_language_store(redis: RedisInterface):
+    bot = MockedAsyncTeleBot(token="")
+    example_user = tg.User(id=161, is_bot=False, first_name="Piotr", last_name="Kropotkin", language_code="ru")
+
+    menu = Menu(
+        text={
+            Language.RU: "заголовок",
+            Language.EN: "header",
+        },
+        config=MenuConfig(
+            back_label={Language.RU: "назад", Language.EN: "back"}, lock_after_termination=False, is_text_html=False
+        ),
+        menu_items=[
+            MenuItem(label={Language.RU: "один", Language.EN: "one"}, terminator="1"),
+            MenuItem(label={Language.RU: "два", Language.EN: "two"}, terminator="2"),
+        ],
+    )
+    language_store = LanguageStore(
+        redis=redis,
+        bot_prefix="1111test",
+        supported_languages=[Language.RU, Language.EN],
+        default_language=Language.EN,
+    )
+
+    menu_handler = MenuHandler(
+        bot_prefix="testing",
+        menu_tree=menu,
+        language_store=language_store,
+    )
+
+    async def on_menu_termination(context: TerminatorContext) -> None:
+        pass
+
+    menu_handler.setup(bot, on_terminal_menu_option_selected=on_menu_termination)
+
+    await menu_handler.start_menu(bot, example_user)
+    assert len(bot.method_calls) == 1
+    all_send_message_kw = extract_full_kwargs(bot.method_calls["send_message"])
+    assert len(all_send_message_kw) == 1
+    send_message_kw = all_send_message_kw[0]
+    reply_markup = send_message_kw.pop("reply_markup")
+    assert send_message_kw == {
+        "chat_id": 161,
+        "text": "заголовок",
+        "parse_mode": "HTML",
+    }
+    assert reply_markup.to_dict() == {
+        "inline_keyboard": [
+            [{"text": "один", "callback_data": "terminator:0"}],
+            [{"text": "два", "callback_data": "terminator:1"}],
+        ]
+    }
+    bot.method_calls.clear()
+
+    await language_store.set_user_language(example_user, Language.EN)
+
+    await menu_handler.start_menu(bot, example_user)
+    assert len(bot.method_calls) == 1
+    all_send_message_kw = extract_full_kwargs(bot.method_calls["send_message"])
+    assert len(all_send_message_kw) == 1
+    send_message_kw = all_send_message_kw[0]
+    reply_markup = send_message_kw.pop("reply_markup")
+    assert send_message_kw == {
+        "chat_id": 161,
+        "text": "header",
+        "parse_mode": "HTML",
+    }
+    assert reply_markup.to_dict() == {
+        "inline_keyboard": [
+            [{"text": "one", "callback_data": "terminator:0"}],
+            [{"text": "two", "callback_data": "terminator:1"}],
+        ]
+    }
+    bot.method_calls.clear()
+
+    async def press_button(callback_data: str):
+        update_json = {
+            "update_id": 19283649187364,
+            "callback_query": {
+                "id": 40198734019872364,
+                "chat_instance": "wtf is this",
+                "from": example_user.to_dict(),
+                "data": callback_data,
+                "message": {
+                    "message_id": 11111,
+                    "from": example_user.to_dict(),
+                    "chat": {
+                        "id": 420,
+                        "type": "private",
+                    },
+                    "date": int(datetime.now().timestamp()),
+                    "text": "menu message placeholder",
+                },
+            },
+        }
+        await bot.process_new_updates([tg.Update.de_json(update_json)])  # type: ignore
